6edaeddf008e9adcf87e743c4e940daafe22db8f
songdaolai <2797257510@qq.com>
2025-03-06 17:12:07 +0800

    哥 这是新的页面

---
+++ @latest/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+
+<feed />
+</template>
+<script>
+import feed from './components/feed.vue'
+
+export default {
+  components: {
+    feed
+  }
+}
+
+
+</script>
---
+++ @latest/src/components/feed.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="head">
+        <div class="frist1">
+            <div class="logo">
+                <img src="" alt="未加载出来">
+            </div>
+            <div class="sosuo">
+                <input type="text" placeholder="搜索">
+                <button>搜索</button> 
+            </div> 
+            <div class="yonghu">
+    
+            </div>
+        </div>
+        <div class="frist2">
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+        </div>
+        <div class="tupian">
+    
+        </div>
+    </div>
+</template>
+<script>
+       
+    
+    
+</script>
+<style scoped>
+.sosuo{
+    display: flex;
+  align-items: center;
+}    
+    
+
+
+</style>
+
